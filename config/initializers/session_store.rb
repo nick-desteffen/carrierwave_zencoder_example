14929ab67118e490dc8060d7c429661f9b16f151
Nick DeSteffen <nick.desteffen@gmail.com>
2011-09-13 23:01:59 -0500

    Configuring flash session Rack middleware

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,9 @@ CarrierwaveZencoderExample::Application.config.session_store :cookie_store, key:
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # CarrierwaveZencoderExample::Application.config.session_store :active_record_store
+
+Rails.application.config.middleware.insert_before(
+  ActionDispatch::Session::CookieStore,
+  FlashSessionCookieMiddleware,
+  Rails.application.config.session_options[:key]
+)
