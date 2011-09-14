7e7658e7c11902da275f79848b50c6cb1618f7fc
Nick DeSteffen <nick.desteffen@gmail.com>
2011-09-13 23:08:26 -0500

    Mounting video uploader

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,8 @@ class Video < ActiveRecord::Base
   attr_accessible :attachment
  
   validates_presence_of :attachment
+  
+  mount_uploader :attachment, VideoUploader
  
   def processed!
     update_attribute(:processed, true)
