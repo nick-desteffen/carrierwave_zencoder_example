133a6ccce9fe37e2f004d07548de855c9ca7fbb2
Nick DeSteffen <nick.desteffen@gmail.com>
2011-09-13 22:53:38 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveZencoderExample::Application.config.secret_token = 'd0069f71528382f0132e43494f8ee5f0065648de1dee906e9d3396e7c7d76617e3f4d31b2206970dd5657c5032645366cdabbec92ada36a564185874a631edea'
